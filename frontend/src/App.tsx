812311c84f2233e433d87d893d68d8b720ea2d16
defint-wq <defint.exe@gmail.com>
2026-04-17 16:16:33 +0800

    run

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,12 @@ export const App = () => {
   return (
     <div>
       <p className="text-3xl text-red-500">{count}</p>
-      <button className="rounded-lg" onClick={() => setcount(count + 1)}>Add</button>
+      <button
+        className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition"
+        onClick={() => setcount(count + 1)}
+      >
+        Add
+      </button>
     </div>
   );
 };
